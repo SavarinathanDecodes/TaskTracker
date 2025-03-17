f5eab767fd0a6d40fc55cfd158e4588cb34f57d8
savarinathan.s <savarinathan.s@utthunga.com>
2025-03-17 11:12:51 +0530

    middleware implementation

---
+++ Api/Server/Helper/AppMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Server.Helper
+{
+    public class AppMiddleware
+    {
+
+        #region Member variables
+
+        private readonly RequestDelegate _next;
+
+        #endregion
+
+
+        #region Constractor
+
+        public AppMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                await HandleExceptionMessageAsync(context, ex).ConfigureAwait(false);
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static Task HandleExceptionMessageAsync(HttpContext context, Exception exception)
+        {
+            var response = new ApiResponse<string>("InternalServerError", exception.Message, "MIDDLEWARE-EXCEPTION");
+            var result = JsonConvert.SerializeObject(response);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return context.Response.WriteAsync(result);
+        }
+
+        #endregion
+    }
+}
